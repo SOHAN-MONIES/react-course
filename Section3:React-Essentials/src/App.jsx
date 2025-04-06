1c1f4815410e4f4c2a40d6bfb916b1091cdef45a
SOHAN-MONIES <anon.marvin.sohan@gmail.com>
2025-04-06 18:07:54 +0530

    add all

---
+++ Section3:React-Essentials/src/App.jsx
@@ -0,0 +1,56 @@
+import Img from "./assets/react-core-concepts.png";
+
+import { CORE_CONCEPTS } from "./data";
+
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+function getRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+function Header() {
+  const description = reactDescriptions[getRandomInt(2)];
+  return (
+    <header>
+      <img src={Img} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
+
+function CoreConcepts(props) {
+  return (
+    <li>
+      <img src={props.img} alt="" />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            {CORE_CONCEPTS.map((core) => (
+              <CoreConcepts
+                key={core.title}
+                img={core.image}
+                title={core.title}
+                description={core.description}
+              />
+            ))}
+          </ul>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
