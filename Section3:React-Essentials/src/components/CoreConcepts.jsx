c89f536d278122c8b90d3d4896087696854ba93b
SOHAN-MONIES <anon.marvin.sohan@gmail.com>
2025-04-12 12:50:50 +0530

    message

--- Section3:React-Essentials/src/components/CoreConcepts.jsx
+++ Section3:React-Essentials/src/components/CoreConcepts.jsx
@@ -1,10 +1,22 @@
-function CoreConcepts(props) {
+import React from "react";
+import CoreConcept from "../components/CoreConcept";
+import { CORE_CONCEPTS } from "../data";
+
+function CoreConcepts() {
   return (
-    <li>
-      <img src={props.img} alt="" />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
-    </li>
+    <section id="core-concepts">
+      <h2>Core Concepts</h2>
+      <ul>
+        {CORE_CONCEPTS.map((core) => (
+          <CoreConcept
+            key={core.title}
+            image={core.image}
+            title={core.title}
+            description={core.description}
+          />
+        ))}
+      </ul>
+    </section>
   );
 }
 
